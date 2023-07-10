944b641d9ea03b80ba0151dcece0e5bb7ab07da8
Davi Antonaji <davi@antonaji.com.br>
2023-07-10 11:46:22 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function formatCEP(cep) {
+    cep = cep.replace("-", "")
+    cep = cep.replace(".", "")
+    return cep.replace(/^(\d{5})(\d{3})$/, "$1-$2");
+  }
+  
+  function consultarCEP(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.erro) {
+          alert("CEP não encontrado. Por favor, digite um CEP válido.");
+        } else {
+          document.getElementById("cidade").value = data.localidade;
+          document.getElementById("estado").value = data.uf;
+          document.getElementById("endereco").value = data.logradouro;
+  
+          document.getElementById("formulario").classList.add("hidden");
+          document.getElementById("resultado").classList.remove("hidden");
+        }
+      })
+      .catch(error => console.log(error));
+  }
+  
+  document.getElementById("consultar").addEventListener("click", function() {
+    var cep = document.getElementById("cep").value;
+    if (cep.length === 8) {
+      cep = formatCEP(cep);
+      consultarCEP(cep);
+    } else {
+      alert("Por favor, digite um CEP válido.");
+    }
+  });
+  
+  document.getElementById("consultar-novo").addEventListener("click", function() {
+    document.getElementById("resultado").classList.add("hidden");
+    document.getElementById("formulario").classList.remove("hidden");
+  });
+  
